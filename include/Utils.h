c85acacee4fc476820e5e3e6cca247826f539aac
FrancescoGigli <65950922+FrancescoGigli@users.noreply.github.com>
2025-01-07 17:33:02 +0100

    Add files via upload

--- include/Utils.h
+++ include/Utils.h
@@ -7,13 +7,9 @@
 #include <fstream>
 #include "Point.h"
 
-// Funzione per leggere punti da un file CSV
+
 std::vector<Point> read_points_from_csv(const std::string& filename);
 
-// Funzione per aprire un file di output
-std::ofstream open_output_file(const std::string& filename);
-
-// Funzione per scrivere output su stream e file
 void write_output(std::ostream& console, std::ofstream& file, const std::string& message);
 
 #endif // UTILS_H
